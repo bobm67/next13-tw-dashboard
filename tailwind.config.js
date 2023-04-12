6a7d62733192d69d711acbc1175e6e0185a49d1d
bobm67 <58613511+bobm67@users.noreply.github.com>
2023-04-11 21:57:20 -0400

    feat: add sidebar and top cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+		'headwind.classRegex': {
+			javascriptreact:
+				'(?:\\bclassName\\s*=\\s*[\\"\\\']([_a-zA-Z0-9\\s\\-\\:\\/]+)[\\"\\\'])|(?:\\btw\\s*`([_a-zA-Z0-9\\s\\-\\:\\/]*)`)',
+			html: '\\bclass\\s*=\\s*[\\"\\\']([_a-zA-Z0-9\\s\\-\\:\\/]+)[\\"\\\']',
+		},
+	},
+	plugins: [require('tailwindcss-debug-screens')],
 }
